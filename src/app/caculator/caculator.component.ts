5f70e23dbf5b3063353cfbbefc89175a21bfc66e
phamlananh1 <phamlananh12011995@gmail.com>
2019-03-15 09:11:59 +0700

    file

---
+++ src/app/caculator/caculator.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caculator',
+  templateUrl: './caculator.component.html',
+  styleUrls: ['./caculator.component.css']
+})
+export class CaculatorComponent {
+
+  output: number;
+  first: number;
+  second: number;
+  operator = '+';
+  onFirstChange(value) {
+    this.first = Number(value);
+  }
+  onSecondChange(value) {
+    this.second = Number(value);
+  }
+  onSelectChange(value) {
+    this.operator = value;
+  }
+  calculate() {
+    switch (this.operator) {
+      case '+':
+        this.output = this.first + this.second;
+        break;
+      case '-':
+        this.output = this.first - this.second;
+        break;
+      case '*':
+        this.output = this.first * this.second;
+        break;
+      case '/':
+        this.output = this.first / this.second;
+        break;
+    }
+  }
+
+}
